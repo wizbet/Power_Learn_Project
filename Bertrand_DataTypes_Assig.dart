022bff06c439d2559cec7316af020942928fd373
SMARTY SYSTEMS <79816163+wizbet@users.noreply.github.com>
2024-02-29 21:22:21 +0000

    Dart Data Types Assignment
    Dart Data Types Assignment

---
+++ Bertrand_DataTypes_Assig.dart
@@ -0,0 +1,18 @@
+void main() {
+  //int datatype declearation
+  int loanAmount = 45645;
+  print(loanAmount); // print loanAmount to user
+
+//double datatype declearation
+  double fees = 534.43;
+  print(fees); // print fees to user
+
+//list datatype declearation
+  List names = ['john', 'Moses', 'Mohammed', 'Lisa'];
+  print(names[3]); //print name with index 3 = Lisa
+
+// string datatype declearation
+  String firstName = "Bertrand";
+
+  print(firstName); // print firstName = Bertrand
+}
