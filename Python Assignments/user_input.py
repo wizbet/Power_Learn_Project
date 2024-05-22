8c37a50350e8b89a885375d16080a21efd6a1375
SMARTY SYSTEMS <79816163+wizbet@users.noreply.github.com>
2024-05-22 16:12:29 +0000

    update

---
+++ Python Assignments/user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+
+age = int(input("Enter your age:"))
+location = input ("Enter your location: ")
+
+print('Hello!', name ,'you are', age,'years old' ,'and live in', location)
--- user_input.py
+++
@@ -1,6 +0,0 @@
-name = input("Enter your name: ")
-
-age = int(input("Enter your age:"))
-location = input ("Enter your location: ")
-
-print('Hello!', name ,'you are', age,'years old' ,'and live in', location)
